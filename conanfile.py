a27b8811942e0580d86da028fe0136a6d483c105
Steve Noonan <17674551+stevenoonan@users.noreply.github.com>
2023-07-07 12:01:32 -0400

    Use conan for imgui, glfw, vulkan

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
+from conan.tools.files import copy
+
+
+class ImGuiExample(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("tomlplusplus/3.3.0")
+        self.requires("glfw/3.3.8")
+        self.requires("imgui/cci.20230105+1.89.2.docking")
+        self.requires("vulkan-loader/1.3.239.0")
+        self.requires("vulkan-headers/1.3.239.0")
+
+    def generate(self):
+
+        imgui_res_bindings = os.path.join(self.dependencies["imgui"].package_folder,
+            "res", "bindings")
+
+        imterm_conan_imgui_bindings = os.path.join(self.source_folder, "deps", "conan", "imgui", "bindings")
+        
+        copy(self, "*glfw*", imgui_res_bindings, imterm_conan_imgui_bindings)
+        #copy(self, "*opengl3*", imgui_res_bindings, imterm_conan_imgui_bindings)
+        copy(self, "*vulkan*", imgui_res_bindings, imterm_conan_imgui_bindings)
+        copy(self, "*win32*", imgui_res_bindings, imterm_conan_imgui_bindings)
+
+    def layout(self):
+        cmake_layout(self)
